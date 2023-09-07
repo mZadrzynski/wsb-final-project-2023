2831c574ccb00597f1bcfebff2fb8657f81ce11e
Marcin Zadrzyński <warshipdesign@gmail.com>
2023-09-07 20:35:26 +0200

    #14 Autoryzacja i uwierzytelniania #15 przygowanie formularza zgłoszeń #17 walidacja zgłoszeń #21 w trakcie

--- src/main/java/com/wsb/wsbfinalproject2022/authority/PersonController.java
+++ src/main/java/com/wsb/wsbfinalproject2022/authority/PersonController.java
@@ -5,20 +5,63 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
+import java.util.List;
+import java.util.Optional;
+
+@Controller
+@RequestMapping("/users")
 public class PersonController {
+
+    private final PersonRepository personRepository;
+    private final RoleRepository roleRepository;
+
+    public PersonController(PersonRepository personRepository, RoleRepository roleRepository) {
+        this.personRepository = personRepository;
+        this.roleRepository = roleRepository;
+
+    }
+
 
     //@RequestMapping(value="/logout", method = RequestMethod.GET)
     @GetMapping("/logout")
-    public String logoutPage (HttpServletRequest request, HttpServletResponse response) {
+    public String logoutPage(HttpServletRequest request, HttpServletResponse response) {
         Authentication auth = SecurityContextHolder.getContext().getAuthentication();
-        if (auth != null){
+        if (auth != null) {
             new SecurityContextLogoutHandler().logout(request, response, auth);
         }
         return "redirect:/login"; //You can redirect wherever you want, but generally it's a good practice to show login screen again.
     }
 
+    @GetMapping("/users")
+    ModelAndView users() {
+        ModelAndView modelAndView = new ModelAndView("users/users");
+        modelAndView.addObject("persons",personRepository.findAll());
+        modelAndView.addObject("roles",roleRepository.findAll());
+        return modelAndView;
+    }
+
+    @GetMapping("/create")
+    //@Secured("ROLE_CREATE_USER")
+    ModelAndView create() {
+        ModelAndView modelAndView = new ModelAndView("users/create");
+        modelAndView.addObject("role", new Role());
+        modelAndView.addObject("person", new Person());
+        Optional<Role> roles = roleRepository.findByName(RoleType.ROLE_ADMIN);
+        return modelAndView;
+    }
+
+    @PostMapping("/save")
+    ModelAndView saveUser(@ModelAttribute Person person, @ModelAttribute Role role) {
+        ModelAndView modelAndView = new ModelAndView();
+        personRepository.save(person);
+        roleRepository.save(role);
+        return modelAndView;
+    }
 }
